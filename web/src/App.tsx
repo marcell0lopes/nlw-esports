a29da1d0149a0f6ea2e201d4adfac11fb4a61f02
Marcello Lopes <marcell0lopes@outlook.com>
2022-09-13 08:59:55 -0300

    static web landing page

---
+++ web/src/App.tsx
@@ -0,0 +1,81 @@
+import './styles/main.css'
+import {MagnifyingGlassPlus} from 'phosphor-react'
+
+import logoImg from './assets/logo-nlw-esports.svg'
+
+export function App() {
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} />
+      
+      <h1 className="text-6xl text-white font-black mt-20">Seu <span className='bg-nlw-gradient bg-clip-text text-transparent'>duo</span> está aqui.</h1>
+      
+      <div className="grid grid-cols-6 gap-6 mt-16">
+
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/game-1.png" alt="League of Legends" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className="font-bold text-white block">League of Legends</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+
+        <a href=""  className='relative rounded-lg overflow-hidden'>
+          <img src="/game-2.png" alt="League of Legends" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className="font-bold text-white block">Dota 2</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+
+        <a href=""  className='relative rounded-lg overflow-hidden'>
+          <img src="/game-3.png" alt="League of Legends" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className="font-bold text-white block">CS:GO</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+
+        <a href=""  className='relative rounded-lg overflow-hidden'>
+          <img src="/game-4.png" alt="League of Legends" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className="font-bold text-white block">Apex Legends</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/game-5.png" alt="League of Legends" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className="font-bold text-white block">Fortnite</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/game-6.png" alt="League of Legends" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className="font-bold text-white block">World of Warcraft</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+
+      </div>
+
+    <div className='pt-1 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden'>
+      <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+        <div><strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
+        <span className="text-zinc-400">Publique um anúncio para encontrar novos players!</span></div>
+
+        <button className='py-3 px-4 text-white bg-violet-500 hover:bg-violet-600 rounded flex items-center gap-3'>
+          <MagnifyingGlassPlus size={24}/>
+          Publicar anúncio
+        </button>
+      </div>
+    </div>
+
+    </div>
+  )
+}
+
+
---
+++ web/tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.tsx', './index.html'],
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 33.33%, #43E7AD 60.66%, #E1D55D 93.33%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
+      }
+    }
+  },
+  plugins: []
+}
